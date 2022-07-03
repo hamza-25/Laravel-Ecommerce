1bbfcebcc3e4ea66d1fbf284cd85f305b00a2201
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2022-07-03 15:14:06 +0100

    Update oneOrder.php

--- app/Models/oneOrder.php
+++ app/Models/oneOrder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class oneOrder extends Model
 {
-    protected $fillable=['serial',"total_price", "status"];
+       protected $fillable=['serial',"total_price", "status","user_id"];
     use HasFactory;
     public function order(){
         return $this->hasMany(order::class,"one_order");
