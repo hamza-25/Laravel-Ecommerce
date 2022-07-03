eab751cd2b6af15a40da874d9f95d7631d15f93d
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2022-07-03 15:15:12 +0100

    Update order.php

--- app/Models/order.php
+++ app/Models/order.php
@@ -9,7 +9,7 @@ class order extends Model
 {
     protected $casts=["products_id"=>"array"];
     use HasFactory;
-    protected  $fillable= ['order_num'];
+        protected  $fillable= ['order_num','qty','price','user_id','product_id','status','address_id'];
 
     public function user(){
         return $this->belongsTo(user::class);
